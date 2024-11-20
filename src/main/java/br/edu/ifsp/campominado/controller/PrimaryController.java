a6f24ac2ef2b004e4b1a1751b9ca0d56ca27b97c
FilipeFiorio <filipefiorio@hotmail.com>
2024-11-20 16:23:49 -0300

    Feito o menu iniciar do jogo

--- src/main/java/br/edu/ifsp/campominado/controller/PrimaryController.java
+++ src/main/java/br/edu/ifsp/campominado/controller/PrimaryController.java
@@ -2,7 +2,10 @@ package br.edu.ifsp.campominado.controller;
 
 import java.io.IOException;
 import br.edu.ifsp.campominado.app2.App;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 
 public class PrimaryController {
 
@@ -10,4 +13,53 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
+    @FXML
+    private MenuItem audio;
+
+    @FXML
+    private MenuItem credits;
+
+    @FXML
+    private MenuItem exit;
+
+    @FXML
+    private Button highScore;
+
+    @FXML
+    private MenuItem howToPlay;
+
+    @FXML
+    private Button jogar;
+
+    @FXML
+    void enableAudio(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitProgram(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showCredits(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showHighScores(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showHowToPlay(ActionEvent event) {
+
+    }
+
+    @FXML
+    void startGame(ActionEvent event) {
+
+    }
+
 }
