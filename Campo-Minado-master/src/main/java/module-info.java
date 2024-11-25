0b681f3bba78da0ec8ee1ea022ccead6dc206304
Filipe Fiorio <filipefiorio@hotmail.com>
2024-11-25 18:36:11 -0300

    Delete Campo-Minado-master directory

--- Campo-Minado-master/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module br.edu.ifsp.campominado {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens br.edu.ifsp.campominado.app2 to javafx.fxml;
-    opens br.edu.ifsp.campominado.controller to javafx.fxml;
-    exports br.edu.ifsp.campominado.app2;
-}
